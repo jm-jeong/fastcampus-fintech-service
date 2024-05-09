c564526d0ed6df381fc5f4c0c6e3bed22dfd0d82
jaemin <jjm7655@gmail.com>
2024-05-09 16:51:18 +0900

    LocalDateTime 수정

--- api-server/src/main/java/com/fastcampus/apiserver/service/DemoService.java
+++ api-server/src/main/java/com/fastcampus/apiserver/service/DemoService.java
@@ -4,13 +4,14 @@ import com.fastcampus.common.enums.CodeEnum;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 @Service
 public class DemoService {
     public String save() {
         System.out.println(CodeEnum.SUCCESS.getCode());
-        LocalDate seoulNow = LocalDate.now(ZoneId.of("Asia/Seoul"));
+        LocalDateTime seoulNow = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
         return seoulNow.toString();
     }
 
