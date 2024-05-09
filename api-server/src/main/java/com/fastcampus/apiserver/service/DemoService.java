6d219ca894c263b86e81c66348bf38fa5df43fed
jaemin <jjm7655@gmail.com>
2024-05-09 16:47:29 +0900

    Controller Test

--- api-server/src/main/java/com/fastcampus/apiserver/service/DemoService.java
+++ api-server/src/main/java/com/fastcampus/apiserver/service/DemoService.java
@@ -3,11 +3,15 @@ package com.fastcampus.apiserver.service;
 import com.fastcampus.common.enums.CodeEnum;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 @Service
 public class DemoService {
     public String save() {
         System.out.println(CodeEnum.SUCCESS.getCode());
-        return "save";
+        LocalDate seoulNow = LocalDate.now(ZoneId.of("Asia/Seoul"));
+        return seoulNow.toString();
     }
 
     public String find() {
