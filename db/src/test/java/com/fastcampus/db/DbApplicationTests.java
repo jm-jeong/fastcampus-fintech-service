df6ae20cc5d2b572ba31ae3ab3f4ea4ce940b5ff
jaemin <jjm7655@gmail.com>
2024-05-17 13:42:23 +0900

    build: application.yaml 설정 및 Application class

--- db/src/test/java/com/fastcampus/db/DbApplicationTests.java
+++ db/src/test/java/com/fastcampus/db/DbApplicationTests.java
@@ -1,10 +1,10 @@
-package com.fastcampus.apiserver;
+package com.fastcampus.db;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiServerApplicationTests {
+class DbApplicationTests {
 
 	@Test
 	void contextLoads() {
