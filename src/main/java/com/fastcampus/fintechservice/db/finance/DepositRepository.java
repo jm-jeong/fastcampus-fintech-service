fd138ec2ae734427e30b523bbf64a53964613ac2
jaemin <jjm7655@gmail.com>
2024-06-07 00:40:42 +0900

    #38-feat: Deposit, Saving 상세 정보에서 5개 금융상품 리스트 query 추가

--- src/main/java/com/fastcampus/fintechservice/db/finance/DepositRepository.java
+++ src/main/java/com/fastcampus/fintechservice/db/finance/DepositRepository.java
@@ -1,6 +1,13 @@
 package com.fastcampus.fintechservice.db.finance;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepositRepository extends JpaRepository<Deposit, String> {
+<<<<<<< Updated upstream
+=======
+
+	List<Deposit> findTop5ByKorCoNm(String korCoNm);
+>>>>>>> Stashed changes
 }
