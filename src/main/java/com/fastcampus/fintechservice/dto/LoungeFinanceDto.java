23b998f5cf3ddf630fce6486fe8e9debe314a021
lesu <46406965+leeesu@users.noreply.github.com>
2024-06-08 05:21:28 +0900

    #41-feat: deposit와 saving이 구분되도록 response 메소드 생성

---
+++ src/main/java/com/fastcampus/fintechservice/dto/LoungeFinanceDto.java
@@ -0,0 +1,68 @@
+package com.fastcampus.fintechservice.dto;
+
+import com.fastcampus.fintechservice.common.ImageConverter;
+import com.fastcampus.fintechservice.db.finance.Deposit;
+import com.fastcampus.fintechservice.db.finance.Saving;
+import com.fastcampus.fintechservice.db.finance.enums.FinProductType;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.IOException;
+
+@Builder
+@Getter
+public class LoungeFinanceDto {
+
+    private String financeId;//depostId or savingId
+    private FinProductType finProductType;//financeType
+    private String finPrdtNm;//금융상품명
+
+    private String korCoNm;//은행명
+    private String imageBase64;//은행이미지 인코딩 값
+
+    private Double intrRateShow;//저축 금리[소수점2자리]
+    private Double intrRate2Show;//최고 우대금리[소수점 2자리]
+
+    private Integer joinMin;//가입기간 최솟값
+    private Integer joinMax;//가입기간 최댓값
+
+
+    public static LoungeFinanceDto depositFrom (Deposit deposit) throws IOException {
+        Integer min = deposit.getDepositOptions().stream().mapToInt(
+                depositOption -> Integer.parseInt(depositOption.getSaveTrm())).min().orElseThrow();
+        Integer max = deposit.getDepositOptions().stream().mapToInt(
+                depositOption -> Integer.parseInt(depositOption.getSaveTrm())).max().orElseThrow();
+
+
+        return LoungeFinanceDto.builder()
+                .financeId(deposit.getDepositId())
+                .finProductType(FinProductType.DEPOSIT)
+                .finPrdtNm(deposit.getFinPrdtNm())
+                .korCoNm(deposit.getKorCoNm())
+                .imageBase64(ImageConverter.encodeBase64(deposit.getBank().getImageName()))
+                .intrRateShow(deposit.getIntrRateShow())
+                .intrRate2Show(deposit.getIntrRate2Show())
+                .joinMin(min)
+                .joinMax(max)
+                .build();
+    }
+
+    public static LoungeFinanceDto savingFrom(Saving saving) throws IOException {
+        Integer min = saving.getSavingOptions().stream().mapToInt(
+                savingOption -> Integer.parseInt(savingOption.getSaveTrm())).min().orElseThrow();
+        Integer max = saving.getSavingOptions().stream().mapToInt(
+                savingOption -> Integer.parseInt(savingOption.getSaveTrm())).max().orElseThrow();
+
+        return LoungeFinanceDto.builder()
+                .financeId(saving.getSavingId())
+                .finProductType(FinProductType.SAVING)
+                .finPrdtNm(saving.getFinPrdtNm())
+                .korCoNm(saving.getKorCoNm())
+                .imageBase64(ImageConverter.encodeBase64(saving.getBank().getImageName()))
+                .intrRateShow(saving.getIntrRateShow())
+                .intrRate2Show(saving.getIntrRate2Show())
+                .joinMin(min)
+                .joinMax(max)
+                .build();
+    }
+}
