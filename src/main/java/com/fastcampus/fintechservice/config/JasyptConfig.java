ad719b519497e95eb133ec9c83fa06f1d4a0e99b
jaemin <jjm7655@gmail.com>
2024-06-07 16:38:28 +0900

    build: jasypt 설정 해제

--- src/main/java/com/fastcampus/fintechservice/config/JasyptConfig.java
+++ src/main/java/com/fastcampus/fintechservice/config/JasyptConfig.java
@@ -1,36 +1,34 @@
 package com.fastcampus.fintechservice.config;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.jasypt.iv.RandomIvGenerator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableEncryptableProperties
+// @Configuration
+// @EnableEncryptableProperties
 public class JasyptConfig {
 
-    @Value("${jasypt.encryptor.password}")
-    private String SECRET_KEY;
+	@Value("${jasypt.encryptor.password}")
+	private String SECRET_KEY;
 
-    private static final String ALGORITHM = "PBEWithHMACSHA512AndAES_256";
+	private static final String ALGORITHM = "PBEWithHMACSHA512AndAES_256";
 
-    @Bean("jasyptStringEncryptor")
-    public StringEncryptor stringEncryptor() {
-        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(SECRET_KEY);
-        config.setAlgorithm("PBEWithHMACSHA512AndAES_256");
-        config.setIvGenerator(new RandomIvGenerator());
-        config.setKeyObtentionIterations("1000");
-        config.setPoolSize("1");
-        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-        config.setStringOutputType("base64");
-        encryptor.setConfig(config);
+	@Bean("jasyptStringEncryptor")
+	public StringEncryptor stringEncryptor() {
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword(SECRET_KEY);
+		config.setAlgorithm("PBEWithHMACSHA512AndAES_256");
+		config.setIvGenerator(new RandomIvGenerator());
+		config.setKeyObtentionIterations("1000");
+		config.setPoolSize("1");
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setStringOutputType("base64");
+		encryptor.setConfig(config);
 
-        return encryptor;
-    }
+		return encryptor;
+	}
 }
