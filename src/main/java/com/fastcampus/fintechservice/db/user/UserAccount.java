e75afe95ab2ddf11c295f8fca06cde46458a5b22
jaemin <jjm7655@gmail.com>
2024-06-07 16:21:20 +0900

    #43-feat: Liked-UserAccount 연관관계 user_id 수정, like유무 쿼리 추가

--- src/main/java/com/fastcampus/fintechservice/db/user/UserAccount.java
+++ src/main/java/com/fastcampus/fintechservice/db/user/UserAccount.java
@@ -5,7 +5,15 @@ import java.util.Objects;
 import com.fastcampus.fintechservice.db.AuditingFields;
 import com.fastcampus.fintechservice.db.user.enums.UserRole;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,6 +35,7 @@ public class UserAccount extends AuditingFields {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id", nullable = false)
 	private Long id;
 
 	@Column(nullable = false, length = 30)
@@ -46,13 +55,13 @@ public class UserAccount extends AuditingFields {
 
 	@Builder
 	public UserAccount(Long id, String name, String email,
-					   String password, UserRole userRole, String kakaoId) {
+		String password, UserRole userRole, String kakaoId) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.password = password;
 		this.userRole = userRole;
-		this.kakaoId =	kakaoId;
+		this.kakaoId = kakaoId;
 	}
 
 	@Override
