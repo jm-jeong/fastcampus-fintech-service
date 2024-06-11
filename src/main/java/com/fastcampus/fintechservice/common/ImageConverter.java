dbcdbc9434fe1160e6eb30ab1705ec4dde2d9f86
jaemin <jjm7655@gmail.com>
2024-06-11 16:13:35 +0900

    #69-feat: s3 주소 세팅 및 image url convert 추가

--- src/main/java/com/fastcampus/fintechservice/common/ImageConverter.java
+++ src/main/java/com/fastcampus/fintechservice/common/ImageConverter.java
@@ -4,10 +4,23 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
 public class ImageConverter {
+	private static String awsS3Url;
+
+	@Value("${aws.s3-url}")
+	public void setAwsS3Url(String awsS3Url) {
+		ImageConverter.awsS3Url = awsS3Url;
+	}
+
 	public static String encodeBase64(String imageName) throws IOException {
 		// 은행 이미지 base64로 인코딩
 		String imageBase64 = "";
@@ -21,5 +34,9 @@ public class ImageConverter {
 		imageBase64 = Base64.getEncoder().encodeToString(bytes);
 
 		return imageBase64;
+	}
+
+	public static String convertImageUrl(String imageName) {
+		return awsS3Url.concat("/images/").concat(imageName).concat(".png");
 	}
 }
