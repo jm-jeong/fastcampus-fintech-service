16650cff1e1c4785646bae29079789d474369bff
jaemin <jjm7655@gmail.com>
2024-06-13 15:33:34 +0900

    fix: 맞춤추천 isLiked false 수정

--- src/main/java/com/fastcampus/fintechservice/controller/RecommendationController.java
+++ src/main/java/com/fastcampus/fintechservice/controller/RecommendationController.java
@@ -1,14 +1,17 @@
 package com.fastcampus.fintechservice.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fastcampus.fintechservice.common.api.Api;
+import com.fastcampus.fintechservice.dto.UserDto;
 import com.fastcampus.fintechservice.dto.request.RecommendationRequest;
 import com.fastcampus.fintechservice.dto.response.RecommendationResponse;
 import com.fastcampus.fintechservice.service.RecommendationService;
+import com.fastcampus.fintechservice.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,11 +20,14 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/recommendation")
 public class RecommendationController {
 	private final RecommendationService recommendationService;
+	private final UserService userService;
 
 	@GetMapping
 	public Api<RecommendationResponse> recommendation(RecommendationRequest recommendationRequest,
-		@RequestParam(required = false, defaultValue = "5", value = "size") int size) {
+		@RequestParam(required = false, defaultValue = "5", value = "size") int size,
+		Authentication authentication) {
+		UserDto userDto = userService.loadUserByEmail(authentication.getName());
 
-		return Api.OK(recommendationService.getRecommendationList(recommendationRequest, size));
+		return Api.OK(recommendationService.getRecommendationList(recommendationRequest, size, userDto));
 	}
 }
--- src/main/java/com/fastcampus/fintechservice/service/RecommendationService.java
+++ src/main/java/com/fastcampus/fintechservice/service/RecommendationService.java
@@ -11,7 +11,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.fastcampus.fintechservice.db.finance.DepositRepository;
 import com.fastcampus.fintechservice.db.finance.SavingRepository;
+import com.fastcampus.fintechservice.db.liked.LikedRepository;
 import com.fastcampus.fintechservice.dto.RecommendationDto;
+import com.fastcampus.fintechservice.dto.UserDto;
 import com.fastcampus.fintechservice.dto.request.RecommendationRequest;
 import com.fastcampus.fintechservice.dto.response.RecommendationResponse;
 
@@ -25,9 +27,10 @@ import lombok.extern.slf4j.Slf4j;
 public class RecommendationService {
 	private final DepositRepository depositRepository;
 	private final SavingRepository savingRepository;
+	private final LikedRepository likedRepository;
 
 	public RecommendationResponse getRecommendationList(RecommendationRequest recommendationRequest,
-		int size) {
+		int size, UserDto currentUser) {
 		// Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(Sort.Direction.DESC, "intrRateShow"));
 		Integer savingStart = recommendationRequest.getSavingStart();
 		Integer savingEnd = recommendationRequest.getSavingEnd();
@@ -39,16 +42,22 @@ public class RecommendationService {
 					savingEnd, savingGoal, savingType, size)
 				.stream()
 				.map(RecommendationDto::fromSaving)
+				.map(it -> it.setLiked(likedRepository.existsByIdAndSavingId(currentUser.id(), it.getFinanceId())))
 				.collect(Collectors.toList());
 			return new RecommendationResponse(savingList, new ArrayList<>(),
 				savingList);
 		} else if (savingType.equals("F")) {
 			List<RecommendationDto> depositList = depositRepository.findBySaveTrmUpDownAndMaxLimit(savingStart,
-				savingEnd, savingGoal, size).stream().map(RecommendationDto::fromDeposit).collect(Collectors.toList());
+					savingEnd, savingGoal, size)
+				.stream()
+				.map(RecommendationDto::fromDeposit)
+				.map(it -> it.setLiked(likedRepository.existsByIdAndDepositId(currentUser.id(), it.getFinanceId())))
+				.collect(Collectors.toList());
 			List<RecommendationDto> savingList = savingRepository.findBySaveTrmUpDownAndMaxLimit(savingStart,
 					savingEnd, savingGoal, savingType, size)
 				.stream()
 				.map(RecommendationDto::fromSaving)
+				.map(it -> it.setLiked(likedRepository.existsByIdAndSavingId(currentUser.id(), it.getFinanceId())))
 				.collect(Collectors.toList());
 
 			ArrayList<RecommendationDto> allList = new ArrayList<>();
